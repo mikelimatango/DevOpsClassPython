b64739e37856cc65380b1d106e386504565c8864
StreetCone <43349541+StreetCone@users.noreply.github.com>
2018-10-17 22:06:06 -0500

    Delete Request_counts.py
    Added code to main.py

--- Request_counts.py
+++
@@ -1,9 +0,0 @@
-print("\nREQUEST COUNTS:")
-requests = sorted(requests.items(), key=operator.itemgetter(1))
-
-
-print("totalRequests:",totalRequests)
-print("Percentage of unsuccessful requests:","{0:.2}%".format((errorRequests)/(errorRequests + redirectedRequests + successRequests)*100))
-print("Percentage of redirected requests:","{0:.2}%".format((redirectedRequests)/(errorRequests + redirectedRequests + successRequests)*100))
-print("Least requested:", requests[0])
-print("Most requested:", requests[-1])
