278aec91da0ef76826128118859f03c121bfa841
mikelimatango <43384273+mikelimatango@users.noreply.github.com>
2018-10-17 22:52:01 -0500

    Updated main.py with more comments

--- main.py
+++ main.py
@@ -46,10 +46,11 @@ for x in open(LOCAL_FILE).readlines():
 		pass
 
 print("\nREQUEST COUNTS:")
+#sorts the dictionary, sorts the request by number of times requested
 requests = sorted(requests.items(), key=operator.itemgetter(1))
 
-
-print("totalRequests:",totalRequests)
+#prints answers to questions
+print("Total Requests:",totalRequests)
 print("Percentage of unsuccessful requests:","{0:.2}%".format((errorRequests)/(errorRequests + redirectedRequests + successRequests)*100))
 print("Percentage of redirected requests:","{0:.2}%".format((redirectedRequests)/(errorRequests + redirectedRequests + successRequests)*100))
 print("Least requested:", requests[0])
